cc7e32fc124c8fb662b8c36313282b794b8c4670
qfl1ck32 <qfl1ck32@yahoo.com>
2020-11-13 17:10:59 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -36,7 +36,7 @@
                     $oldSessId = sendQuery('select sessionId from users where ' . $type . ' = ?', $usernameEmail)[0];
                     session_id($oldSessId['sessionId']);
                     session_destroy();
-                    return Status(-5, "I have disconnected the guy with sessId: " . $oldSessId . " !!!!!!!!!");
+                    return Status(-5, "I have disconnected the guy with sessId: " . $oldSessId['sessionId'] . " !!!!!!!!!");
                     session_start();
                 }
 
