4eeaa2e288a6b52777616540fb0c8bd87c3932f6
qfl1ck32 <qfl1ck32@yahoo.com>
2020-11-14 09:19:33 +0200

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -3,9 +3,12 @@
 
     session_start();
 
-    $sessId = sendQuery('select sessionId from users where id = unhex(?);', $_SESSION['id']);
+    $sessId = sendQuery('select sessionId as sid from users where id = unhex(?);', $_SESSION['id'])[0]['sid'];
+
     if (session_id() == $sessId)
         sendQuery('update users set sessionId = 0 where id = unhex(?);', $_SESSION['id']);
+
+    session_destroy();
 
     return header('location: /Index.php');
 ?>
